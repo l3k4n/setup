924b2a507a5b9d3707befceaed98471d4873e7f1
lekan <alowoojalekan@gmail.com>
2024-07-19 20:23:54 -0500

    allow installing single script

--- install.sh
+++ install.sh
@@ -17,6 +17,21 @@ if [ `id -u` -eq 0 ]; then
     exit 1
 fi
 
-for file in scripts/* ; do
-	run_script $file
-done
+if [ $# -eq 0 ]; then
+	# install all if no args
+	for file in scripts/* ; do
+		run_script $file
+	done
+	exit 1
+else
+	# only install args provided
+	for arg in "$@"; do
+		file="scripts/${arg}.sh"
+
+		if [ -e "$file" ]; then
+			run_script $file
+		else
+			echo "->  ${ERR}No install script found for: '$file'${NC}"
+		fi
+	done
+fi
