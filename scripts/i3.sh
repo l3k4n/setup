ac8a31f097ebbf339a4884d0c2afa85e95cc71b2
lekan <alowoojalekan@gmail.com>
2024-07-19 20:36:05 -0500

    add install script for i3

---
+++ scripts/i3.sh
@@ -0,0 +1,12 @@
+# install i3
+sudo apt-get -qq -y install i3
+
+# install i3blocks
+git clone https://github.com/vivien/i3blocks
+cd i3blocks
+./autogen.sh
+./configure
+make
+make install
+cd ..
+rm -rf i3blocks
