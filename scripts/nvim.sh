d9ee899a865b90de903a37212bd76ab17d2c6b7c
lekan <alowoojalekan@gmail.com>
2024-07-19 20:35:38 -0500

    cleanup downloaded files while installing

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -2,6 +2,7 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.
 rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 echo 'export PATH="$PATH:/opt/nvim-linux64/bin"' >> ~/.bashrc
+rm nvim-linux64.tar.gz
 
 # for nvim clipboard
 sudo apt-get -qq -y install xclip
